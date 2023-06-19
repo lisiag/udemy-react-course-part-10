3ef57919774c089e6a9cc34753bb2d3c30343641
Lisia Grocott <lisiagrocott@gmail.com>
2023-06-19 20:52:16 +1200

    s11 p161 refactor: create Input custom component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import React, { useContext, useState, useEffect, useReducer } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
 import AuthContext from "../../store/auth-context";
 
 const emailReducer = (currentState, action) => {
@@ -96,34 +97,8 @@ const Login = () => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input isValid={emailState.isValid} id="email" label="E-mail" type="email" value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler}/>
+        <Input isValid={passwordState.isValid} id="password" label="Password" type="password" value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler}/>
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
